809ed0e95afed7c5e57a68b1a040474b6c9ea2a3
erikg27 <erik.gibboney@gmail.com>
2026-08-31 04:23:33 -0600

    working timer that can count down from 10.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
 import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function MyButton() {
   const [isRunning, setRunning] = useState(false);
+  const [secondsLeft, setSecondsLeft] = useState(10);
+
+  useEffect(() => {
+    let id;
+
+    if (isRunning) {
+      id = setInterval(() => setSecondsLeft((prev) => prev - 1), 1000);
+    }
+
+    return () => {
+      clearInterval(id); //clean up
+    };
+  }, [isRunning]); // rerun this effect whenever isRunning changes
+
   function handleClick() {
-    //You can respond to events by
-    //declaring event handler functions inside your components:
     setRunning(!isRunning);
   }
+
+  let label;
+
+  if (isRunning) {
+    label = "Stop";
+  } else {
+    label = "Start";
+  }
+
   return (
-    <>
-      <button onClick={handleClick}>Start:{isRunning.toString()}</button>
-    </>
+    <div className="button">
+      <button onClick={handleClick}>{label}</button>
+      <div className="timer"> {secondsLeft} </div>
+    </div>
   );
 }
 
 export default function MyApp() {
   return (
-    <>
+    <div>
       <h1> LOCK IN </h1>
       <div>
-        <MyButton className="startButton" />
+        <MyButton />
       </div>
-    </>
+    </div>
   );
 }
